8a5f1e6b6b42d8ef76504f315c4b8e1252d3851d
Anthony Woodard <anthony.woodard@gmail.com>
2014-12-10 13:10:10 -0600

    Code cleanup

--- src/main/java/com/anthonywoodard/keyshed/util/ConsoleStringTable.java
+++ src/main/java/com/anthonywoodard/keyshed/util/ConsoleStringTable.java
@@ -14,8 +14,8 @@ public class ConsoleStringTable {
     int _row, _colum;
 
     public Index (int r, int c) {
-        _row= r;
-        _colum= c;
+        _row = r;
+        _colum = c;
     }
 
     @Override
@@ -26,7 +26,7 @@ public class ConsoleStringTable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Index other= (Index) obj;
+        Index other = (Index) obj;
         if (_colum != other._colum)
             return false;
         if (_row != other._row)
@@ -36,40 +36,40 @@ public class ConsoleStringTable {
 
     @Override
     public int hashCode () {
-      final int prime= 31;
-      int result= 1;
-      result= prime * result + _colum;
-      result= prime * result + _row;
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + _colum;
+      result = prime * result + _row;
       return result;
     }
   }
 
-  Map<Index, String> _strings= new HashMap<ConsoleStringTable.Index, String>();
-  Map<Integer, Integer> _columSizes= new HashMap<Integer, Integer>();
+  Map<Index, String> _strings = new HashMap<ConsoleStringTable.Index, String>();
+  Map<Integer, Integer> _columSizes = new HashMap<Integer, Integer>();
 
-  int _numRows= 0;
-  int _numColumns= 0;
+  int _numRows = 0;
+  int _numColumns = 0;
 
   public void addString (int row, int colum, String content) {
-      _numRows= Math.max(_numRows, row + 1);
-      _numColumns= Math.max(_numColumns, colum + 1);
+      _numRows = Math.max(_numRows, row + 1);
+      _numColumns = Math.max(_numColumns, colum + 1);
 
-      Index index= new Index(row, colum);
+      Index index = new Index(row, colum);
       _strings.put(index, content);
 
       setMaxColumnSize(colum, content);
   }
 
   private void setMaxColumnSize (int colum, String content) {
-      int size= content.length();
-      Integer currentSize= _columSizes.get(colum);
+      int size = content.length();
+      Integer currentSize = _columSizes.get(colum);
       if (currentSize == null || (currentSize != null && currentSize < size)) {
           _columSizes.put(colum, size);
       }
   }
 
   public int getColumSize (int colum) {
-      Integer size= _columSizes.get(colum);
+      Integer size = _columSizes.get(colum);
       if (size == null) {
           return 0;
       } else {
@@ -78,8 +78,8 @@ public class ConsoleStringTable {
   }
 
   public String getString (int row, int colum) {
-      Index index= new Index(row, colum);
-      String string= _strings.get(index);
+      Index index = new Index(row, colum);
+      String string = _strings.get(index);
       if (string == null) {
           return "";
       } else {
@@ -88,16 +88,16 @@ public class ConsoleStringTable {
   }
 
   public String getTableAsString (int padding) {
-      String out= "";
-      for (int r= 0; r < _numRows; r++) {
-          for (int c= 0; c < _numColumns; c++) {
-              int columSize= getColumSize(c);
-              String content= getString(r, c);
+      String out = "";
+      for (int r = 0; r < _numRows; r++) {
+          for (int c = 0; c < _numColumns; c++) {
+              int columSize = getColumSize(c);
+              String content = getString(r, c);
               int pad= c == _numColumns - 1 ? 0 : padding;
-              out+= Strings.padEnd(content, columSize + pad, ' ');
+              out += Strings.padEnd(content, columSize + pad, ' ');
           }
           if (r < _numRows - 1) {
-              out+= "\n";
+              out += "\n";
           }
       }
       return out;
